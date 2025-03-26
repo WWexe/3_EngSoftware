48f0b19f227becf96763aca60e9bb0638bcf8b35
WWexe <oeningvinicius@gmail.com>
2025-03-25 21:40:11 -0300

    Teste de Software

---
+++ Teste de Software/desconto-app/src/test/java/br/com/desconto/Service/DescontoServiceTest.java
@@ -0,0 +1,16 @@
+package br.com.desconto.Service.;
+
+import br.com.desconto.service.DescontoService;
+import junit.framework.TestCase;
+import junit.framework.Test;
+import br.com.desconto.model.Produto;
+
+
+public class DescontoServiceTest extends TestCase {
+    @Test
+    public void testeCalcularDesconto() {
+        DescontoService service = new DescontoService();
+
+        Produto produto = new Produto()
+    }
+}
